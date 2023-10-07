94287ba9ec66e58d94af892c3757a058c54d3fbc
Andrius Busma <45238190+andriusbusma@users.noreply.github.com>
2023-10-07 14:39:57 +0300

    Implement chat gpt endpoint.

--- back/main.py
+++ back/main.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from bardapi import Bard
+from flask import request
+from flask import jsonify
+
+import openai
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    token = 'bwjdzYkKZLN0hs4mGfKomne_wtWygro3QufxGZWycxLb3Ch9X-MSPwU32zChOPoCtQGA4Q.'
-    bard = Bard(token=token)
-    return bard.get_answer("How to cure cancer?")['content']
+@app.route("/request", methods=['POST'])
+def main():
+    openai.api_key = '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'
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": request.json['question']}])
+    return jsonify(
+        answer=completion.choices[0].message.content
+    )

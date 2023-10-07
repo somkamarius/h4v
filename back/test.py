ee934f48bb3725d93d8201eba4f03023fb2a23de
NotATit <titas.masys@gmail.com>
2023-10-07 16:44:37 +0300

    Added fixes.

--- back/test.py
+++ back/test.py
@@ -2,4 +2,4 @@ from Prompts.PromptReader import PromptReader
 from StableDiffusionApi.StableDiffusionApi import StableDiffusionApi
 import os
 
-print(StableDiffusionApi.GetGeneratedImageFromText("Vytautas the great", '', 1, 'no', 'no', None))
+print(StableDiffusionApi.GetGeneratedImageFromText("19 century lady, stylized, black and white", '', 1, 'no', 'no', None))
